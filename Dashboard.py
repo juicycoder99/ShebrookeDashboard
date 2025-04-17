98a399e73fa314959a594c5e2dcd120e4f462d47
CODE JHUSSAINI <67003703+juicycoder99@users.noreply.github.com>
2025-04-16 20:36:49 -0400

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import os
+os.system("pip install xgboost")
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
